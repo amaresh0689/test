786e1fce02675611da9add626a61939500b7bad5
amaresh0689 <40541131+amaresh0689@users.noreply.github.com>
2021-06-25 12:18:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ provider "aws" {
   region = "eu-central-1"
 }
  
-resource "aws_instance" "kaeptn-eichhorn" {
+resource "aws_instance" "codepipes" {
   ami = "ami-13b8337c"
   instance_type = "t2.micro"
+  name = "codepipes test"
 }
