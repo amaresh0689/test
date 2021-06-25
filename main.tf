53e2c527c09df6bdea70ffd8c739d2f624f61d70
amaresh0689 <40541131+amaresh0689@users.noreply.github.com>
2021-06-25 12:24:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,5 +5,14 @@ provider "aws" {
 resource "aws_instance" "codepipes" {
   ami = "ami-13b8337c"
   instance_type = "t2.micro"
+  tags
+  {
   name = "codepipes test"
+  }
 }
+
+resource "aws_s3_bucket" "cldcvr-s3" {
+  bucket = "cldcvr"
+  depends_on = ["aws_instance.codepipes"] # Dependency to EC2-Instance
+}
+ 
