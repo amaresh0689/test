c44f481ce57238fcae472fce0d09423563d766b4
amaresh0689 <40541131+amaresh0689@users.noreply.github.com>
2021-06-25 12:27:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,10 +5,6 @@ provider "aws" {
 resource "aws_instance" "codepipes" {
   ami = "ami-13b8337c"
   instance_type = "t2.micro"
-  tags
-  {
-  name = "codepipes test"
-  }
 }
 
 resource "aws_s3_bucket" "cldcvr-s3" {
