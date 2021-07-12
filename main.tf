78ebdd71c092e47726eeff025ac7d8e1331ec191
amaresh0689 <40541131+amaresh0689@users.noreply.github.com>
2021-07-12 11:59:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,6 @@ resource "aws_vpc" "cldcvr_vpc" {
 resource "aws_subnet" "cldcvr_subnet" {
   vpc_id            = aws_vpc.cldcvr_vpc.id
   cidr_block        = "172.16.10.0/24"
-  availability_zone = "eu-central-1"
 
   tags = {
     Name = "cldcvr-subnet"
