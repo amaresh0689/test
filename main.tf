2bc2122a7db3bb264ead5f4233d6f86dbb02f097
amaresh0689 <40541131+amaresh0689@users.noreply.github.com>
2021-07-12 11:42:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,44 @@
 provider "aws" {
   region = "eu-central-1"
 }
- 
+
+resource "aws_vpc" "cldcvr_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "cldcvr-vpc"
+  }
+}
+
+resource "aws_subnet" "cldcvr_subnet" {
+  vpc_id            = aws_vpc.cldcvr_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "eu-central-1"
+
+  tags = {
+    Name = "cldcvr-subnet"
+  }
+}
+
+resource "aws_network_interface" "cldcvr_ni" {
+  subnet_id   = aws_subnet.cldcvr_subnet.id
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
 resource "aws_instance" "codepipes" {
   ami = "ami-13b8337c"
   instance_type = "t2.micro"
-}
+  
+  network_interface 
+  {
+    network_interface_id = aws_network_interface.cldcvr_ni.id
+    device_index         = 0
+  }
+ }
 
 resource "aws_s3_bucket" "cldcvr-s3" {
   bucket = "cldcvr"
